249e2e1af5d90e25814a96967d836bdfc3d13c48
wangyi <2851813754@qq.com>
2022-02-16 21:51:55 +0800

    chore: app-todolist页面,app页面，abpout页面，home页面

---
+++ src/pages/About.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: wangyi
+ * @Description: 
+ * @Date: 2022-02-16 21:27:22
+ * @LastEditTime: 2022-02-16 21:27:23
+ */
+import React, { FC } from 'react';
+
+const About:FC =() => {
+  return (
+    <div>
+      About
+    </div>
+  );
+}
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: wangyi
+ * @Description: 
+ * @Date: 2022-02-16 21:27:14
+ * @LastEditTime: 2022-02-16 21:28:40
+ */
+import React, { FC } from 'react';
+
+const Home:FC = () => {
+  return (
+    <div>
+      Home
+    </div>
+  );
+}
+
+export default Home;
