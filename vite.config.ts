84ed8357893081205409f05a0a24282ced97feb4
wangyi <2851813754@qq.com>
2022-03-03 16:08:12 +0800

    config🔧:  vite配置更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginImp from 'vite-plugin-imp'
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    
+  plugins: [
+    react(),
+    vitePluginImp()
+  ],
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', "ts"],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
   }
 })
