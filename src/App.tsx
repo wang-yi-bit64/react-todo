a3b30e814d9ad64bbd90a2a615fd56f109d0dff1
wangyi <2851813754@qq.com>
2022-03-03 16:09:58 +0800

    config🔧:  入口文件修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 /*
  * @Author: wangyi
- * @Description: 
+ * @Description:
  * @Date: 2022-02-16 21:29:22
- * @LastEditTime: 2022-02-16 21:40:34
+ * @LastEditTime: 2022-03-03 14:48:23
  */
-import React,{ FC } from 'react';
+import React, { FC } from 'react';
+import {observer} from "mobx-react-lite"
+import { ConfigProvider } from "antd"
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from '@/pages/Home';
+import About from '@/pages/About';
+import useStore from "@/store";
 // 项目入口
 const App: FC = () => {
+  const store = useStore();
   return (
-    <BrowserRouter>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-      </Routes>
-
-    </BrowserRouter>
+    <ConfigProvider>
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider>
   );
 }
 
-export default App;
+export default observer(App);
