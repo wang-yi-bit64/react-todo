df1657547fa03ca63d4df3e37cc3d68647dfea1e
wangyi <2851813754@qq.com>
2022-03-03 18:32:19 +0800

    refactor🎨:  入口文件更新

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,36 @@
  * @Author: wangyi
  * @Description:
  * @Date: 2022-02-16 21:29:22
- * @LastEditTime: 2022-03-03 14:48:23
+ * @LastEditTime: 2022-03-03 18:01:33
  */
 import React, { FC } from 'react';
-import {observer} from "mobx-react-lite"
+import { Provider } from "mobx-react"
+import { observer } from "mobx-react-lite"
 import { ConfigProvider } from "antd"
 import { BrowserRouter, Route, Link, Routes } from 'react-router-dom'
 import Home from '@/pages/Home';
 import About from '@/pages/About';
-import useStore from "@/store";
+import Todo from "@/pages/Todo";
+import useStores from "@/hooks/useStores";
 // 项目入口
 const App: FC = () => {
-  const store = useStore();
+  const store = useStores();
+  console.log(store.settings)
   return (
-    <ConfigProvider>
-      <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </ConfigProvider>
+    <Provider>
+      <ConfigProvider>
+        <BrowserRouter>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/todo">Todo</Link>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/todo' element={<Todo />} />
+          </Routes>
+        </BrowserRouter>
+      </ConfigProvider>
+    </Provider>
   );
 }
 
